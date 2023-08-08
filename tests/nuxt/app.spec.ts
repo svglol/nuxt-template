b73554aa7c880896dc407f6596f55d3138d90c06
Matthew Trotman <savageguy@gmail.com>
2023-08-09 11:25:30 +1200

    test: rewrite app test

---
+++ tests/nuxt/app.spec.ts
@@ -0,0 +1,20 @@
+import app from '~/app.vue'
+import { describe, it, expect } from 'vitest'
+import { mountSuspended } from 'nuxt-vitest/utils'
+
+describe('app config', () => {
+  it('is empty', () => {
+    expect(useAppConfig()).toEqual({})
+  })
+})
+
+describe('app', () => {
+  it('app can be mounted', async () => {
+    const component = await mountSuspended(app, { route: '/' })
+    expect(component).toBeTruthy()
+  })
+  it('app has welcome to nuxt text', async () => {
+    const component = await mountSuspended(app, { route: '/' })
+    expect(component.html()).toContain('Welcome to Nuxt!')
+  })
+})
