ce6a55d3db6a1798573a903aaebe8ff8f604afc5
Matthew Trotman <savageguy@gmail.com>
2023-09-06 15:32:37 +1200

    chore: add @nuxt/devtools to nuxt modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['nuxt-vitest'],
+  modules: ['nuxt-vitest', '@nuxt/devtools'],
   devtools: { enabled: true },
 })
