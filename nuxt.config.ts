be508fdd3631a8d35228f13d3f63517ddc1db24d
Matthew Trotman <savageguy@gmail.com>
2024-03-02 20:44:12 +1300

    feat: add nuxt/fonts and nuxt/ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/test-utils/module', '@nuxt/devtools'],
+  modules: [
+    '@nuxt/test-utils/module',
+    '@nuxt/devtools',
+    '@nuxt/fonts',
+    '@nuxt/ui',
+  ],
   devtools: { enabled: true },
+  tailwindcss: {
+    cssPath: '~/assets/global.css',
+  },
 })
