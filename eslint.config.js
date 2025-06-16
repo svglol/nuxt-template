bbfdb6775ac5b3dd9bb92ea67f17ef543d2291f9
Matthew Trotman <savageguy@gmail.com>
2025-06-16 12:48:55 +1200

    chore(deps): add eslint-plugin-better-tailwindcss and update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,28 @@
 import antfu from '@antfu/eslint-config'
+import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss'
 import nuxt from './.nuxt/eslint.config.mjs'
 
 export default antfu(
-
-  { rules: {
-    'vue/block-order': ['error', {
-      order: ['template', 'script', 'style'],
-    }],
-  } },
+  {
+    rules: {
+      'vue/block-order': ['error', {
+        order: ['template', 'script', 'style'],
+      }],
+    },
+  },
+  {
+    plugins: {
+      'better-tailwindcss': eslintPluginBetterTailwindcss,
+    },
+    settings: {
+      'better-tailwindcss': {
+        entryPoint: 'app/assets/css/main.css',
+      },
+    },
+    rules: {
+      ...eslintPluginBetterTailwindcss.configs['recommended-warn'].rules,
+      'better-tailwindcss/multiline': ['off'],
+    },
+  },
   nuxt,
 )
